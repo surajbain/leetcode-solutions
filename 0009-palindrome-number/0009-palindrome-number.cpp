88748d5de8ca5ad399afdd12cf2677cb0b0a1371
Suraj Bain <bainsuraj648@gmail.com>
2026-07-25 22:27:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (65.75%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if (x <0 )
+        return false;
+
+     int original = x;
+     long long rev =0;
+
+     while (x>0) {
+        rev =rev* 10 + x % 10;
+        x /= 10;
+     }
+        return original == rev;
+    }
+};
