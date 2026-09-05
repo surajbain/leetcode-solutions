d67aa9d888de9f439be2d994ad08216dbb900baa
Suraj Bain <bainsuraj648@gmail.com>
2026-09-05 22:18:46 +0530

    Time: 5 ms (51.22%), Space: 14 MB (56.87%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if ( numRows == 1 || numRows >= s.length ()) {
+            return s;
+        }
+        vector<string> rows(numRows);
+
+        int currentRow = 0;
+        bool goingDown = false;
+
+        for( char ch : s) {
+            rows[currentRow] += ch;
+
+            if ( currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            if (goingDown) {
+                currentRow++;
+            } else {
+                currentRow--;
+            }
+        }
+
+        string answer = "";
+
+        for (string row : rows) {
+            answer += row;
+        }
+        return answer;
+    }
+};
