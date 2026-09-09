cdde576b90acac5b40807e6f51593d44d79d6a9b
Suraj Bain <bainsuraj648@gmail.com>
2026-09-09 21:01:06 +0530

    Time: 0 ms (100%), Space: 7.8 MB (58.67%) - LeetHub

---
+++ 0168-excel-sheet-column-title/0168-excel-sheet-column-title.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber) {
+        string result;
+
+        while (columnNumber > 0) {
+            columnNumber--;
+
+            int remainder = columnNumber % 26;
+
+            result += char('A' + remainder);
+
+            columnNumber /= 26;
+        }
+
+        reverse(result.begin(), result.end());
+
+        return result;
+    }
+};
